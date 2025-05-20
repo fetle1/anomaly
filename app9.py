51fb3f39964f1c09f77cca1337c61e59e6b464ce
fetle1 <99641535+fetle1@users.noreply.github.com>
2025-05-20 11:16:44 +0300

    Update app9.py

--- app9.py
+++ app9.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import io
+import subprocess
+subprocess.run(["pip", "install", "--upgrade", "pip"])
 
 # Optional visualization libraries
 import matplotlib.pyplot as plt
