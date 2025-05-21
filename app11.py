550b8c1adaf7412ee57a50f021d4517622c73751
fetle1 <99641535+fetle1@users.noreply.github.com>
2025-05-21 09:09:23 +0300

    Update app11.py

--- app11.py
+++ app11.py
@@ -346,7 +346,7 @@ def rule_based_anomaly_detection():
 
     # BPsys normal range 90-120, BPDias normal 60-80
     conditions = [
-        (df["systolic_bp"] < df["diastolic_bp"])
+        (df["systolic_bp"] < df["diastolic_bp"])]
 
     
     anomalies = df[np.logical_or.reduce(conditions)]
